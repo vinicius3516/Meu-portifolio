60dc5da4d52a4becee9b810c0f348afbf695b71f
devvinny <costavinicius692@gmail.com>
2023-12-07 20:52:58 -0300

    alterando secao/habilidades

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-var ul = document.querySelector(".navegador ul");
-var menu = document.querySelector(".menu i");
+const ul = document.querySelector(".navegador ul");
+const menu = document.querySelector(".menu i");
+const habilitando = document.querySelectorAll(".habili");
 
 function menuShow() {
   ul.classList.toggle("open");
 }
+
+habilitando.forEach(function (habili) {
+  habili.addEventListener("click", function () {
+    habili.classList.toggle("ativo");
+  });
+});
